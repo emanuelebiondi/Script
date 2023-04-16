d34d0d0046d4d3a4cf3cf11596581f7ffa580d65
GiuCala <68949769+GiuCala@users.noreply.github.com>
2023-04-16 23:38:58 +0100

    Update analytics.sql
    Continuo primo anal

--- analytics.sql
+++ analytics.sql
@@ -25,7 +25,7 @@ CREATE PROCEDURE ConsigliIntervento(IN CodEdificio_f INT)
             WHERE v.Edificio = CodEdificio_f
         ),
         SensoriAllarmati AS(
-            SELECT s.CodSensore, s.Muro, s.Tipologia, s.Soglia s.Longitudine, s.Latitudine, a.TimeStamp
+            SELECT s.CodSensore, s.Muro, s.Tipologia, s.Soglia s.Longitudine, s.Latitudine, a.TimeStamp, a.ValoreSuperamento
             FROM Sensore s INNER JOIN Alert a ON s.CodSensore = a.Sensore
             WHERE (
                 s.Tipologia = "Giroscopio" OR
@@ -34,8 +34,20 @@ CREATE PROCEDURE ConsigliIntervento(IN CodEdificio_f INT)
             )
         ),
         AlertInteressati AS(
-            SELECT sm.CodSensore, sm.Tipologia, sm.Soglia, sm.Longitudine, sm.Latitudine, sm.TimeStamp
+            SELECT sm.CodSensore, sm.Tipologia, sm.Soglia, sm.Longitudine, sm.Latitudine, sm.TimeStamp, sm.ValoreSuperamento
             FROM MuriScelti ms INNER JOIN SensoriAllarmati sa ON sa.Muro = ms.CodMuro
+        ),
+        AlertRecenti AS(
+            SELECT *, (
+                (ABS(at.ValoreSuperamento-at.Soglia)/at.Soglia)*100 + (ABS(at.ValoreSuperamento-at.Soglia)/at.Soglia)*100*fattoreDiRischio
+            ) AS DannoPerc
+            FROM AlertInteressati at
+            WHERE at.TimeStamp >= ALL (
+                SELECT at1.TimeStamp
+                FROM AlertInteressati at1
+                WHERE at1.CodSensore = at.CodSensore
+            )
         )
-    END $$
+        
+    END $$
 DELIMITER;
