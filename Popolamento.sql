74e84d4ad189a115e651491f9c95c997cf42ef38
GiuCala <68949769+GiuCala@users.noreply.github.com>
2023-04-16 17:57:28 +0100

    Update Popolamento.sql
    Update tabelle popolamento, mancano le coordinate delle calamità

--- Popolamento.sql
+++ Popolamento.sql
@@ -6,11 +6,11 @@ use SmartConstructionCompany;
 ----------------------------------
 insert into AreaGeografica (Nome)
 values 			
-	('Nord-Ovest'), 
-	('Nord-Est'), 
+	('Nord'), 
+	('Sardegna'), 
 	('Centro'), 
 	('Sud'), 
-	('Isole');
+	('Sicilia');
 				
 ----------------------------------
 -- RISCHIO
@@ -76,5 +76,54 @@ values
 -- EDIFICIO
 ----------------------------------
 -- ?? Stato Edificio
-insert into Edificio (Tipologia, DataRealizzazione, Longitudine, Latitudine, AreaGeografica)
-VALUES('Villetta', '1987-07-13', 43.640578, 10.290558, 'Centro');
+insert into Edificio (Tipologia, DataRealizzazione, StatoEdificio, Longitudine, Latitudine, AreaGeografica)
+VALUES 
+		('Villetta', '1987-07-13', 80, 43.640578, 10.290558, 'Centro');
+		('Condominio', '1989-05-21', 97, 38.153594, 15.660652, 'Sud');
+
+
+
+----------------------------------
+-- RISCHIO
+----------------------------------
+INSERT INTO Rischio (CodRischio, Tipo, Coefficiente, AreaGeografica)
+VALUES
+		(01, Sismico, 40, Centro);
+		(02, Idraulico, 60, Centro);
+		(03, Sismico, 20, Sud);
+		(04, Sismico, 10, Sicilia);
+		(05, Idraulico, 5, Sardegna);
+		(06, Idraulico, 55, Sicilia);
+		(07, Idraulico, 19, Sud);
+		(08, Sismico, 5, Nord);
+		(09, Idraulico, 37, Nord);
+
+
+
+----------------------------------
+-- CALAMITÀ
+----------------------------------
+INSERT INTO Calamità (AreaGeografica, Tipologia, Data, LivelloIntensita, Longitudine, Latitudine);
+values
+		(Sud, 'Gelamento', '2002-02-03', 3, 43.6, 22);
+		(Nord, 'Valanga', '2020-08-10', 5, 38.9, 22.6);
+		(Centro, 'Frana', '2005-11-22', 1, 35, 18.5);
+		(Sud, 'Alluvione', '2018-02-28', 3, 48, 30);
+		(Nord, 'Terremoto', '2002-06-29', 4, 47, 28);
+		(Centro, 'Inondazione', '2001-10-06', 2);
+		(Sicilia, 'Uragano', '1999-04-24', 6);
+		(Sud, 'Eruzione', '2017-09-13', 9);
+		(Sardegna, 'Terremoto', '2013-10-28', 0);
+		(Sardegna, 'Incendio', '2021-01-15', 4);
+		(Sud, 'Gelamento', '2013-12-30', 3);
+		(Sicilia, 'Valanga', '2019-03-31', 7);
+		(Sardegna, 'Frana', '2000-03-29', 0);
+		(Nord, 'Alluvione', '2014-11-08', 5);
+		(Centro, 'Terremoto', '2009-05-07', 5);
+		(Sicilia, 'Inondazione', '2016-05-29', 4);
+		(Sud, 'Uragano', '1994-05-19', 6);
+		(Sicilia, 'Eruzione', '2008-02-07', 9);
+		(Nord, 'Terremoto', '2022-03-24', 4);
+		(Sardegna, 'Incendio', '1996-07-13', 4);
+		(Centro, 'Gelamento', '2007-08-29', 1);
+		(Sud, 'Valanga', '1996-09-25', 3);
