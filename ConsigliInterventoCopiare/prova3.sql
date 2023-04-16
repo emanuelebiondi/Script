a94de83a09d761bd057c114c5c9a7f6052439bd3
GiuCala <68949769+GiuCala@users.noreply.github.com>
2023-04-17 00:21:23 +0100

    Update prova3.sql

--- ConsigliInterventoCopiare/prova3.sql
+++ ConsigliInterventoCopiare/prova3.sql
@@ -1,99 +1,46 @@
-drop procedure if exists consigli_intervento;
-delimiter :)
-create procedure consigli_intervento(_edificio int, _datetime datetime)
-begin
-	select
-		(prediction-soglia)*coefficiente_muri() as urgenza,
-		if((prediction-soglia)*coefficiente_muri() < soglia_ricostruzione(), 'Consolidamento', 'Ricostruzione') as intervento,
-        'Muro' as tipo_elemento,
-        muro_sensore(SP.id) as id_elemento,
-        costo_costruzione_muro_from_sensore(SP.id) as stima_spesa_crollo,
-        SP.id as id_sensore
-    from (
-		select
-			S.id,
-			S.soglia,
-			linear_best_fit_predict(S.id, _datetime, _datetime-interval 1 week, _datetime+interval 1 week) as prediction
-		from sensore S inner join vano V on S.vano = V.id
-		where V.edificio = _edificio and S.tipo='Allungamento'
-        ) SP
-	where prediction > soglia
- 
-    union
-    
-    select
-		media_triassiale(_edificio, 'Accelerometro', 300, _datetime)*coefficiente_struttura_accelerometro() as urgenza,
-		if(media_triassiale(_edificio, 'Accelerometro', 300, _datetime)*coefficiente_struttura_accelerometro() < soglia_ricostruzione(), 'Consolidamento', 'Ricostruzione') as intervento,
-        'Struttura edificio' as tipo_elemento,
-        null as id_elemento,
-        costo_lavori_edificio(_edificio) as stima_spesa_crollo,
-        null as id_sensore
-        
-	union
-    
-	select
-        media_triassiale(_edificio, 'Giroscopio', 300, _datetime)*coefficiente_struttura_giroscopio() as urgenza,
-		if(media_triassiale(_edificio, 'Giroscopio', 300, _datetime)*coefficiente_struttura_giroscopio() < soglia_ricostruzione(), 'Raddrizzamento', 'Ricostruzione') as intervento,
-        'Struttura edificio' as tipo_elemento,
-        null as id_elemento,
-        costo_lavori_edificio(_edificio) as stima_spesa_crollo,
-        null as id_sensore
-        
-	union
+DROP PROCEDURE IF EXISTS Analytics2;
+DELIMITER $$
+CREATE PROCEDURE Analytics2 (IN CodEdificio VARCHAR(16), OUT valore_finale float)
+BEGIN
 
-    select * from
-		(select
-			(max(M.xOppureUnico)-min(M.xOppureUnico)-offset_escursione())*coefficiente_ambiente_escursione() as urgenza,
-			'Cappotti di isolamento' as intervento,
-			'Vano' as tipo_elemento,
-			V.id as id_elemento,
-			0 as stima_spesa_crollo,
-			S.id as id_sensore
-		from sensore S inner join vano V on S.vano = V.id inner join misura M on M.sensore = S.id
-		where
-			V.edificio = _edificio and
-			S.tipo='Temperatura' and
-			M.timestamp between _datetime-interval 1 week and _datetime
-		group by S.id) TMP
-	where TMP.urgenza >= 0
-    
-    union
-    
-    select * from
-		(select
-			(avg(M.xOppureUnico)-S.soglia)*coefficiente_ambiente_temperatura() as urgenza,
-			'Installazione climatizzazione' as intervento,
-			'Vano' as tipo_elemento,
-			V.id as id_elemento,
-			0 as stima_spesa_crollo,
-			S.id as id_sensore
-		from sensore S inner join vano V on S.vano = V.id inner join misura M on M.sensore = S.id
-		where
-			V.edificio = _edificio and
-			S.tipo='Temperatura' and
-			M.timestamp between _datetime-interval 1 week and _datetime
-		group by S.id) TMP
-    where TMP.urgenza >= 0
-    
-    union
-    
-    select * from 
-		(select
-			(avg(M.xOppureUnico)-S.soglia)*coefficiente_ambiente_umidita() as urgenza,
-			'Installazione deumidificatore' as intervento,
-			'Vano' as tipo_elemento,
-			V.id as id_elemento,
-			0 as stima_spesa_crollo,
-			S.id as id_sensore
-		from sensore S inner join vano V on S.vano = V.id inner join misura M on M.sensore = S.id
-		where
-			V.edificio = _edificio and
-			S.tipo='Umidita' and
-			M.timestamp between _datetime-interval 1 week and _datetime
-		group by S.id) TMP
-    where TMP.urgenza >= 0
+DECLARE Contributo_Sensore FLOAT DEFAULT 30; -- 30 per cento
+DECLARE Contributo_Stato FLOAT DEFAULT 60; -- 60 per cento
+DECLARE Contributo_EventiSismici FLOAT DEFAULT 10; -- 10 per cento
+DECLARE Stringa VARCHAR(60);
 
-    order by urgenza desc;
-    
-end :)
-delimiter ;
+SET Contributo_Sensore = (SELECT SUM(VarPercentuale)*0.3
+						  FROM (SELECT M.ValoreX, M.ValoreY, M.ValoreZ, S.SogliaDiRischio, sqrt(M.ValoreX*M.ValoreX+M.ValoreY*M.ValoreY+M.ValoreZ*M.ValoreZ)/S.SogliaDiRischio AS VarPercentuale
+								FROM misurazionetriassiale M
+									 INNER JOIN
+									 sensore S on M.Sensore = S.Codice
+								     INNER JOIN 
+								     alerttriassiale A on A.MisurazioneTriassiale = M.Codice             
+						WHERE M.Edificio = CodEdificio and S.Grandezza = 'AccelerazioneAngolare') AS z);
+                          
+SET Contributo_Stato = (SELECT If(E.stato is null, 0, E.Stato)/ 3 * 0.6 as VarPercentualeStato
+						FROM Edificio E
+                        WHERE E.Codice = CodEdificio);
+                        
+SET Contributo_EventiSismici = (SELECT EventiEdificio/EventiTotali*0.1
+							    FROM 	(SELECT count(*) as EventiTotali, (select count(*)
+																			FROM EventoCalamitoso E1
+																			INNER JOIN
+																			Edificio E2 ON E2.AreaGeografica = E1.AreaGeografica
+																			WHERE E1.NomeEventoCalamitoso = 'Terremoto' AND E2.Codice = CodEdificio) as EventiEdificio
+								FROM EventoCalamitoso E
+								WHERE E.NomeEventoCalamitoso = 'Terremoto') AS b);
+                                
+SET valore_finale = IFNULL(Contributo_EventiSismici + Contributo_Sensore + Contributo_Stato, 0 );
+
+IF valore_finale = 0 THEN SET Stringa = 'Nessun rischio';
+ELSE IF valore_finale <=3 THEN SET Stringa = 'Danni superficiali';
+ELSE IF valore_finale <=5 THEN SET Stringa = 'Danni strutturali';
+ELSE SET Stringa = 'Danni gravi alla struttura';
+END IF;
+END IF;
+END IF;
+
+SELECT valore_finale as IndiceCalcolato, Stringa as Messaggio;
+
+END $$
+DELIMITER ;
