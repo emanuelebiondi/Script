25ae818d79e09e0516beafe9459d7825df9f0244
Whowasfra <96885071+Whowasfra@users.noreply.github.com>
2023-04-17 21:47:34 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -211,8 +211,13 @@ begin
         select count(*) 
         from Turno T
         where T.CapoCantiere = new.CapoCantiere
+	    AND DAY(New.TimestampInizio) = ALL (
+                    SELECT DAY(T1.TimestampInizio) 
+                    FROM Turno T1
+                    WHERE T1.CapoCantiere = T.CapoCantiere
+                )
     );
-
+    
     set numeromassimolav = (
         select MaxOperai
         from CapoCantiere C
